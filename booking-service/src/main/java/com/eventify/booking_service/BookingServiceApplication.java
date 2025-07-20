d082cfd98a128d289c8a16ca502aad9522d9f5e0
Nuraj250 <nurajshaminda200@gmail.com>
2025-07-20 09:44:42 +0530

    booking_service modify

--- booking-service/src/main/java/com/eventify/booking_service/BookingServiceApplication.java
+++ booking-service/src/main/java/com/eventify/booking_service/BookingServiceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookingServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
-
 }
